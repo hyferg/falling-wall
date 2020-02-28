c6e23cb9747f2212fe2816d0117111a42a1e8e07
cameron <cameronhyferg@gmail.com>
2020-02-28 20:42:26 +0000

    column wall and smooth appear new wall

--- pan-zoom/index.js
+++ pan-zoom/index.js
@@ -2,7 +2,9 @@ window.addEventListener('load', function () {
 	  // The `g4` element is inside svg with `viewBox` and
 	  // it has the initial `transform`. Panzoom should still work fine.
 	  panzoom(document.getElementById('layer1'), {
-        zoomSpeed: 0.010,
+        zoomSpeed: 0.050,
         pinchSpeed: 0.5,
-    });
+        bounds: true
+    }).zoomAbs(
+    );
 });
--- proof-of-concept/index.js
+++ proof-of-concept/index.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function() {
     $('#scene').click(function() {
         d1 = 350;
@@ -34,10 +36,42 @@ $(document).ready(function() {
             .queue(function(next) {
                 // the new texture dimensions is the same size with scaled down assets
                 // this means it's not volume preserving
+                $('#texture-broken').css('display', 'block');
                 $('#texture').hide();
-                $('#texture-broken').show();
                 next();
-            });
+            })
+            .queue(function(next) {
 
+                var svg = document.getElementById('layer1');
+                var center_w = $(window).width()/2;
+                var center_h = $(window).height()/2;
+                panzoom(svg).smoothZoom(
+                    center_w,
+                    center_h+$(window).height()/2,
+                    1.3);
+                next();
+                $('#new-wall').css('opacity', '1.0');
+                $('#new-wall').css('z-index', '1');
+
+
+            })
+        ;
     });
+});
+
+window.addEventListener('load', function () {
+	  // The `g4` element is inside svg with `viewBox` and
+	  // it has the initial `transform`. Panzoom should still work fine.
+    var svg = document.getElementById('layer1');
+    var center_w = $(window).width()/2;
+    var center_h = $(window).height()/2;
+	  var instance = panzoom(svg, {
+        zoomSpeed: 0.050,
+        pinchSpeed: 0.5,
+        bounds: true,
+    }).zoomAbs(
+        center_w,
+        center_h,
+        0.33
+    );
 });
